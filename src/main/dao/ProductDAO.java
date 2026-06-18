c198178e934d9d52e69538572cb64395ef030a3e
Bloody969 <oktaviandaffa52@gmail.com>
2026-06-18 17:05:47 +0700

    Menambahkan implementasi ProductDAO

--- src/main/dao/ProductDAO.java
+++ src/main/dao/ProductDAO.java
@@ -1,5 +1,42 @@
 package main.dao;
 
+import database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import main.model.Barang;
+
 public class ProductDAO {
-    // TODO: Query database untuk tabel products
+
+    public ArrayList<Barang> getAllProducts() {
+
+        ArrayList<Barang> daftarBarang = new ArrayList<>();
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+
+            String sql = "SELECT * FROM products";
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                Barang barang = new Barang(
+                        rs.getString("kode_product"),
+                        rs.getString("nama_product"),
+                        rs.getInt("stok"),
+                        rs.getDouble("harga_jual")
+                );
+
+                daftarBarang.add(barang);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return daftarBarang;
+    }
 }
