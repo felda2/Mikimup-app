7f904b8d6534ddfa7e2b2a63db5b15ce4ed2b55e
Bloody969 <oktaviandaffa52@gmail.com>
2026-06-18 20:13:03 +0700

    Menambahkan query riwayat stok

--- src/main/dao/StockHistoryDAO.java
+++ src/main/dao/StockHistoryDAO.java
@@ -1,5 +1,43 @@
 package main.dao;
 
+import database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import main.model.StockHistory;
+
 public class StockHistoryDAO {
-    // TODO: Query database untuk tabel stock_history
+
+    public ArrayList<StockHistory> getAllHistory() {
+
+        ArrayList<StockHistory> historyList = new ArrayList<>();
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+
+            String sql = "SELECT * FROM stock_history";
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                StockHistory history = new StockHistory(
+                        rs.getInt("id_stock_history"),
+                        rs.getInt("id_product"),
+                        rs.getString("tipe"),
+                        rs.getInt("jumlah"),
+                        rs.getString("keterangan")
+                );
+
+                historyList.add(history);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return historyList;
+    }
 }
